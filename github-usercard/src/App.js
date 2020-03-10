344f323f16c6620bf1b3b0ee81cd8e7a4267e364
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-03-10 13:12:36 -0700

    successfully rendered my github user info

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import axios from 'axios';
+
+import UserCard from './components/UserCard';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {}
+    }
+  }
 
-    </div>
-  );
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/jscheuble')
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          user: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <UserCard user={this.state.user} />
+      </div>
+    );
+  }
 }
 
 export default App;
